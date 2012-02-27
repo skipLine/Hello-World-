2f7aaf0e657757960f9129fe92ac59d020d2c8b5
David Lu <daya_l@qq.com>
2012-02-27 00:01:31 -0800

    update

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+class IndexHandler(webapp.RequestHandler):
+    def get(self):
+        if self.request.url.endswith('/'):
+            path = '%sindex.html'%self.request.url
+        else:
+            path = '%s/index.html'%self.request.url
+
+        self.redirect(path)
+
+    def post(self):
+        self.get()
+
+def main():
+    application = webapp.WSGIApplication([('/.*', IndexHandler)],
+                                         debug=False)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
